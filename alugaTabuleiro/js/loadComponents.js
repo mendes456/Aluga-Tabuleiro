7d534b984eb14aea5111c6f28dfb05133af059d0
Brenda Mendes <mendesbrenda840@gmail.com>
2026-03-07 16:40:18 -0300

    Add files via upload
    Página index (principal)

---
+++ alugaTabuleiro/js/loadComponents.js
@@ -0,0 +1,12 @@
+function loadComponent(id, file) {
+
+fetch(file)
+.then(response => response.text())
+.then(data => {
+document.getElementById(id).innerHTML = data;
+});
+
+}
+
+loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
