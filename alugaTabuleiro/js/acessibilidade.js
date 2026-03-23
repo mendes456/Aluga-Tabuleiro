872ace4a8fae47387506416285e5bdd650b909a2
Brenda Mendes <mendesbrenda840@gmail.com>
2026-03-22 23:58:21 -0300

    Add files via upload

---
+++ alugaTabuleiro/js/acessibilidade.js
@@ -0,0 +1,33 @@
+
+
+let tamanhoFonte = 16;
+
+function aumentarFonte() {
+    tamanhoFonte += 2;
+    document.documentElement.style.fontSize = tamanhoFonte + "px";
+}
+
+function diminuirFonte() {
+    tamanhoFonte -= 2;
+    document.documentElement.style.fontSize = tamanhoFonte + "px";
+}
+
+function fonteNormal() {
+    tamanhoFonte = 16;
+    document.documentElement.style.fontSize = "16px";
+}
+
+// 🌙 MODO ESCURO
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+
+function ativarAcessibilidade() {
+
+    document.querySelector(".btn-dark")?.addEventListener("click", toggleDarkMode);
+    document.querySelector(".btn-aumentar")?.addEventListener("click", aumentarFonte);
+    document.querySelector(".btn-diminuir")?.addEventListener("click", diminuirFonte);
+    document.querySelector(".btn-normal")?.addEventListener("click", fonteNormal);
+
+}
--- alugaTabuleiro/js/loadComponents.js
+++ alugaTabuleiro/js/loadComponents.js
@@ -1,12 +1,38 @@
 function loadComponent(id, file) {
 
-fetch(file)
-.then(response => response.text())
-.then(data => {
-document.getElementById(id).innerHTML = data;
-});
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+
+            // 👇 RODA O SCRIPT SÓ QUANDO O HEADER EXISTIR
+            if (id === "header") {
+                ativarMenu();
+                ativarAcessibilidade(); // 👈 NOVO
+                
+            }
+
+            
+        });
 
 }
+
+function ativarMenu() {
+    const menu = document.getElementById('menu');
+
+    if (menu) {
+        menu.addEventListener('show.bs.collapse', function () {
+            document.body.classList.add('menu-aberto');
+        });
+
+        menu.addEventListener('hide.bs.collapse', function () {
+            document.body.classList.remove('menu-aberto');
+        });
+    }
+}
+
+
+
 
 loadComponent("header", "components/header.html");
 loadComponent("footer", "components/footer.html");
